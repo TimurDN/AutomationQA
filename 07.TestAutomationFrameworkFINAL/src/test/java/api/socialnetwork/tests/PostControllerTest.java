e2a19de01d3b0a385bae9eb958384263ee934c17
Timur Naumov <132722130+TimurDN@users.noreply.github.com>
2023-11-07 21:15:11 +0200

    final framework
    final framework

---
+++ 07.TestAutomationFrameworkFINAL/src/test/java/api/socialnetwork/tests/PostControllerTest.java
@@ -0,0 +1,159 @@
+package api.socialnetwork.tests;
+
+import api.socialnetwork.base.BaseTestSetup;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Tag;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
+
+import static apisocialnetwork.Constants.*;
+import static apisocialnetwork.ErrorMessages.*;
+import static apisocialnetwork.ShowMessages.*;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.testng.Assert.*;
+
+public class PostControllerTest extends BaseTestSetup {
+    Logger logger = Logger.getLogger("");
+
+    @Test
+    @Tag("FHKT-252")
+    public void createPostSuccessfullyTest() {
+
+        createAndRegisterUser();
+        loginUser();
+
+        Response response = createPost();
+
+        int statusCode = response.getStatusCode();
+        String postContent = response.getBody().jsonPath().getString("content");
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertEquals(postContent, POST_DESCRIPTION, ERROR_MESSAGE_POST_DESCRIPTION);
+
+        logger.info(SHOW_MESSAGE_RESPONSE_BODY + response.getBody().asPrettyString());
+        logger.info(SHOW_MESSAGE_POST_CREATED_AND_POST_ID);
+
+        deletePostTearDown();
+
+    }
+
+    @Test
+    @Tag("FHKT-154")
+    public void getAllPostsTest() {
+
+        Response response = showAllPosts();
+
+        int statusCode = response.getStatusCode();
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+
+        logger.info(SHOW_MESSAGE_GET_ALL_POSTS);
+    }
+
+    @Test
+    @Tag("FHKT-108")
+    public void showAllProfilePosts_Successful() {
+
+        createAndRegisterUser();
+        loginUser();
+        createPost();
+
+        Response response = showAllProfilePosts();
+
+        int statusCode = response.getStatusCode();
+        String responseBody = response.getBody().asPrettyString();
+
+        logger.info(SHOW_MESSAGE_RESPONSE_BODY + responseBody);
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertTrue(responseBody.length() > 2, ERROR_MESSAGE_RESPONSE_BODY_EMPTY);
+
+        logger.info(SHOW_MESSAGE_GET_ALL_PROFILE_POSTS);
+
+        deletePostTearDown();
+
+    }
+
+
+    @Test
+    @Tag("FHKT-253")
+    public void editPostsTest() {
+
+        createAndRegisterUser();
+        loginUser();
+        createPost();
+
+        Response response = editProfilePost();
+
+        int statusCode = response.getStatusCode();
+        String responseBody = response.getBody().asString();
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertTrue(responseBody.isEmpty(), ERROR_MESSAGE_RESPONSE_BODY_EMPTY);
+
+        logger.info(SHOW_MESSAGE_EDITED_POST);
+
+        deletePostTearDown();
+    }
+
+    @Test
+    @Tag("FHKT-35")
+    public void likeProfilePostTest() {
+
+        createAndRegisterUser();
+        loginUser();
+        createPost();
+
+        Response response = likePost();
+
+        int statusCode = response.getStatusCode();
+        boolean liked = response.jsonPath().getBoolean("liked");
+
+        String postIdFromResponse = response.jsonPath().getString("postId");
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertEquals(postIdFromResponse, POST_ID, ERROR_MESSAGE_RESPONSE_BODY_POST_ID);
+        assertTrue(liked, ERROR_MESSAGE_LIKED_POST);
+
+        logger.info(SHOW_MESSAGE_POST_LIKED);
+
+        deletePostTearDown();
+
+    }
+
+    @Test
+    @Tag("FHKT-254")
+    public void deletePostsTest() {
+
+        createAndRegisterUser();
+        loginUser();
+        createPost();
+
+        Response response = deletePost();
+
+        int statusCode = response.getStatusCode();
+        String responseBody = response.getBody().asString();
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertTrue(responseBody.isEmpty(), ERROR_MESSAGE_RESPONSE_BODY_EMPTY);
+
+        logger.info(SHOW_MESSAGE_POST_DELETED);
+    }
+    public void deletePostTearDown() {
+        Response response = deletePost();
+
+        int statusCode = response.getStatusCode();
+        String responseBody = response.getBody().asPrettyString();
+
+        assertEquals(statusCode, SC_OK, ERROR_MESSAGE_STATUS_CODE);
+        assertEquals(responseBody, "", ERROR_MESSAGE_RESPONSE_BODY_EMPTY);
+
+        logger.info(SHOW_MESSAGE_POST_DELETED);
+
+    }
+}
+
+
+
+
