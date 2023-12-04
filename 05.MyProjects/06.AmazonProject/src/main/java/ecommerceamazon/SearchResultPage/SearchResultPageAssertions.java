83db651f046117e29af6fec5c505a43cc8b53f45
Timur Naumov <132722130+TimurDN@users.noreply.github.com>
2023-12-05 00:08:50 +0200

    SearchResultPage page object and design added. Minor fixes in homepage object

---
+++ 05.MyProjects/06.AmazonProject/src/main/java/ecommerceamazon/SearchResultPage/SearchResultPageAssertions.java
@@ -0,0 +1,20 @@
+package ecommerceamazon.SearchResultPage;
+
+import ecommerceamazon.BasePage.BasePageAmazon;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+public class SearchResultPageAssertions extends BasePageAmazon {
+    public SearchResultPageAssertions(WebDriver driver) {
+        super(driver, "amazon.homepage");
+    }
+
+    public void assertSearchSubmission(String name){
+        Assertions.assertTrue(driver.getTitle().contains(String.format("%s",name)));
+
+    }
+
+    public void assertSearchedProductByNameHasResults(String productName){
+        actions.assertElementPresentUsingWebElement(searchResultsPageElements().searchedProductInResults(productName));
+    }
+}
