6b90435916ba75777101a9b751630e44e0225eb2
Timur Naumov <132722130+TimurDN@users.noreply.github.com>
2023-12-04 18:34:50 +0200

    minor updates for imports

--- 05.MyProjects/06.AmazonProject/src/test/java/uitests/ecommerceamazon/base/BaseTestAmazon.java
+++ 05.MyProjects/06.AmazonProject/src/test/java/uitests/ecommerceamazon/base/BaseTestAmazon.java
@@ -2,12 +2,7 @@ package uitests.ecommerceamazon.base;
 
 import com.telerikacademy.testframework.CustomWebDriverManager;
 import com.telerikacademy.testframework.UserActions;
-import ecommerceamazon.CheckOutPage.CheckOutPageAmazon;
-import ecommerceamazon.ConfirmOrderPage.ConfirmOrderPageAmazon;
 import ecommerceamazon.HomePage.HomePageAmazon;
-import ecommerceamazon.LoginPage.LoginPageAmazon;
-import ecommerceamazon.ProductsPage.ProductsPageAmazon;
-import ecommerceamazon.RegisterPage.RegisterPageAmazon;
 import ecommerceamazon.ShoppingCartPage.ShoppingCartPageAmazon;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,12 +12,7 @@ public class BaseTestAmazon {
 
     //PAGES
     public static HomePageAmazon homePageLambda;
-    public static CheckOutPageAmazon checkOutPageLambda;
-    public static ConfirmOrderPageAmazon confirmOrderPageLambda;
-    public static RegisterPageAmazon registerPageLambda;
-    public static LoginPageAmazon loginPageLambda;
     public static ShoppingCartPageAmazon shoppingCartPageLambda;
-    public static ProductsPageAmazon productsPageLambda;
 
     @BeforeEach
     public void setUp() {
@@ -30,12 +20,7 @@ public class BaseTestAmazon {
         WebDriver driver = CustomWebDriverManager.CustomWebDriverManagerEnum.INSTANCE.getDriver();
 
         homePageLambda = new HomePageAmazon(driver);
-        checkOutPageLambda = new CheckOutPageAmazon(driver);
-        confirmOrderPageLambda = new ConfirmOrderPageAmazon(driver);
-        registerPageLambda = new RegisterPageAmazon(driver);
-        loginPageLambda = new LoginPageAmazon(driver);
-        shoppingCartPageLambda = new ShoppingCartPageAmazon(driver);
-        productsPageLambda = new ProductsPageAmazon(driver);
+
     }
 
     @AfterEach
