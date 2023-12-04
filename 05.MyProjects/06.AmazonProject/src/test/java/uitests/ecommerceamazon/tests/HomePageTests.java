efa3f2bd4bbc085adae9d64efd8dcc07b5c672da
Timur Naumov <132722130+TimurDN@users.noreply.github.com>
2023-12-04 20:03:28 +0200

    HomePage tests completed, all tests are passing.

--- 05.MyProjects/06.AmazonProject/src/test/java/uitests/ecommerceamazon/tests/HomePageTests.java
+++ 05.MyProjects/06.AmazonProject/src/test/java/uitests/ecommerceamazon/tests/HomePageTests.java
@@ -1,25 +1,81 @@
 package uitests.ecommerceamazon.tests;
 
-import ecommerceamazon.BasePage.BasePageAmazon;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import uitests.ecommerceamazon.base.BaseTestAmazon;
 
 public class HomePageTests extends BaseTestAmazon {
 
+    @BeforeEach
+    public void setupForTests(){
+        homePageAmazon.navigateToPage();
+    }
+
     @Test
     public void homePageNavigatedTest() {
-
-        homePageAmazon.navigateToPage();
         homePageAmazon.homePageAssertions().assertHomePageNavigated();
 
     }
 
     @Test
     public void searchBarVisibleAndClickableTest(){
-        homePageAmazon.navigateToPage();
         homePageAmazon.clickOnEmptySearchbar();
         homePageAmazon.homePageAssertions().assertSearchBarVisibleAndClickable();
+    }
+
+    @Test
+    public void homePageAmazonLogoVisibleTest(){
+        homePageAmazon.homePageAssertions().assertHomePageLogo();
+    }
+
+    @Test
+    public void todayDealsButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertTodaysDealsButton("Today's Deals");
+    }
+
+    @Test
+    public void registryButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertTodaysDealsButton("Registry");
+    }
+
+    @Test
+    public void customerServiceButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertTodaysDealsButton("Customer Service");
+    }
+
+    @Test
+    public void giftCardsButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertTodaysDealsButton("Customer Service");
+    }
+
+    @Test
+    public void sellButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertTodaysDealsButton("Sell");
+    }
+
+    @Test
+    public void accountButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertHomeAccountButton();
+    }
+
+    @Test
+    public void ordersButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertOrdersButton();
+    }
+
+    @Test
+    public void shoppingCartButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertShoppingCartButton();
+    }
+
+    @Test
+    public void languageButtonVisibleTest(){
+        homePageAmazon.homePageAssertions().assertHomeLanguageButton();
+    }
+
+    @Test
+    public void defaultLanguageVisibleTest(){
+        homePageAmazon.homePageAssertions().assertHomeDefaultLanguage("EN");
     }
 
 }
